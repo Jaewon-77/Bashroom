4660b05ad6659f5d507fe7ba1859719e8e96e740
Jaewon-77 <babota0606@gmail.com>
2024-02-18 08:42:04 +0000

    deploy commit3

--- db/migrate/20240204055110_create_shoes_reviews.rb
+++ db/migrate/20240204055110_create_shoes_reviews.rb
@@ -2,14 +2,14 @@ class CreateShoesReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :shoes_reviews do |t|
       
-      t.integer :user_id, null: false, default: ""
-      t.string :title, null: false, default: ""
-      t.text :review, null: false, default: ""
-      t.integer :traction, null: false, default: ""
-      t.integer :cushion, null: false, default: ""
-      t.integer :fit, null: false, default: ""
-      t.integer :support, null: false, default: ""
-      t.integer :weight, null: false, default: ""
+      t.integer :user_id, null: false
+      t.string :title, null: false
+      t.text :review, null: false
+      t.integer :traction, null: false
+      t.integer :cushion, null: false 
+      t.integer :fit, null: false 
+      t.integer :support, null: false
+      t.integer :weight, null: false
 
       t.timestamps
     end
