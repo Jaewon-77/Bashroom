62c1f047325093e87214b8cc956e0b464d3a0db9
EC2 Default User <ec2-user@ip-172-31-19-70.ap-northeast-1.compute.internal>
2024-02-19 14:49:00 +0000

    edit error commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :check_profile_set
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   def check_profile_set
     if current_user
       @is_profile_set = current_user.is_profile_set?
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
 end
