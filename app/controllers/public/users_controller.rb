2c2599cf05c8d3c84ca94c3c7c31d02b4070f08e
Jaewon-77 <babota0606@gmail.com>
2024-02-26 06:42:50 +0000

    final commit5

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,6 +1,6 @@
 class Public::UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def show
     @user = User.find(params[:id])
     @shoes_reviews = @user.shoes_reviews
@@ -43,9 +43,9 @@ class Public::UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :address, :favorite_brand, :email, :favorite_shoes_name, :size, :age, :image)
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :address, :favorite_brand, :email, :favorite_shoes_name, :size, :age, :image )
   end
-  
-  
+
+
 
 end
