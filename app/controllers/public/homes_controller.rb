4a5238c485e43131d44d0d0e66180710d82bd0ea
EC2 Default User <ec2-user@ip-172-31-19-70.ap-northeast-1.compute.internal>
2024-02-26 12:31:07 +0000

    final commit8

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,8 @@
 class Public::HomesController < ApplicationController
   before_action :authenticate_user!, except: [:top, :about]
-  
+
   def top
-    @shoes_reviews = ShoesReview.all
+    @shoes_reviews = ShoesReview.all.order(created_at: "DESC")
   end
 
   def about
